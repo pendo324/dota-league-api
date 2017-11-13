319b421ec97c788928a9195dcd35febcad8dc39b
pendo324 <pendo3247@gmail.com>
2017-11-13 17:22:42 -0500

    - add endpoints for joining a queue and accepting a queue - add socket actions to alert the user that the queue is ready and alert the user that the lobby is ready - TODO: add socket action for alerting the user that the queue has failed (someone declined or did not accept in time)

--- api/lobbies.js
+++ api/lobbies.js
@@ -4,12 +4,15 @@ const db = require('../db');
 const app = express.Router();
 const io = require('./../server').io;
 
+const uuid = require('uuid');
+
 io.on('connection', (socket) => {
-  socket.on('message', (data) => {
+  socket.on('message', async (data) => {
     console.log(data);
   });
 
   socket.on('joinLobby', ({ lobbyId, userId }) => {
+    console.log('test');
     socket.join(`lobby_${lobbyId}`);
     db.joinLobby(lobbyId, userId).then(userResult =>
       io.in(`lobby_${lobbyId}`).emit('userJoined', {
@@ -40,6 +43,46 @@ io.on('connection', (socket) => {
       });
       // socket.broadcast.to(`lobby_${id}`).emit('newMessage', { content: message, senderName: socket.handshake.session.passport.displayName });
     });
+  });
+  
+  socket.on('setTeam', async ({ userId, team, lobbyId }) => {
+    const captain = await db.checkCaptain(socket.handshake.session.passport.user.id, team);
+    if (captain) {
+      const onTeam = await db.userOnTeam(userId);
+      if (!onTeam) {
+        await db.addToTeam(userId, team);
+        io.in(`lobby_${lobbyId}`).emit('setTeam', {
+          user: userId,
+          team
+        });
+      }
+    }
+  });
+
+  socket.on('joinQueue', async () => {
+    db.addUserToQueue(socket.handshake.session.passport.user.id, socket.id);
+    checkQueue();
+  });
+
+  socket.on('leaveQueue', async () => {
+    db.removeUserFromQueue(socket.handshake.session.passport.user.id);
+  });
+
+  socket.on('acceptQueue', async (tempAcceptId) => {
+    db.setAccepted(socket.handshake.session.passport.user.id, tempAcceptId);
+    const accepted = await db.getAccepted().rows;
+    if (accepted.length === 10) {
+      // start picking teams
+    }
+  });
+
+  socket.on('queueTimeout', async (queueId) => {
+    // tell all clients to requeue
+    // queueTimeout is sent when one is waiting too long (15s)
+    // after the 'match ready' modal first appears
+    // the users that do not respond to the modal will be
+    // removed from the queue on the client side
+    io.of(queueId).emit('queueTimeout');
   });
 });
 
@@ -98,5 +141,67 @@ app.get('/lobby/:id', (req, res) => {
     //res.json(ret);
   }).catch(err => console.log(err));
 });
+
+app.post('/joinQueue', async (req, res) => {
+  await db.addUserToQueue(req.user.id, req.body.socketId);
+  checkQueue();
+  res.sendStatus(200);
+});
+  
+app.post('/leaveQueue', (req, res) => {
+  db.removeUserFromQueue(req.user.id);
+  res.sendStatus(200);
+});
+    
+app.post('/acceptQueue', async (req, res) => {
+  const { acceptQueueId } = req.body;
+  console.log(acceptQueueId);
+  await db.setAccepted(req.user.id, acceptQueueId);
+  const accepted = await db.getAccepted(acceptQueueId);
+  if (accepted.rowCount === 1) {
+    // start picking teams
+    // blast all sockets to join a newly created lobby
+    const lobbyId = await db.createLobby();
+    Object.keys(io.sockets.adapter.rooms[acceptQueueId].sockets).forEach((socketId) => {
+      io.sockets.connected[socketId].join(`lobby_${lobbyId}`);
+      io.sockets.connected[socketId].leave(acceptQueueId);
+
+      io.in(`lobby_${lobbyId}`).emit('lobbyReady', { lobbyId });
+
+      db.joinLobby(lobbyId, req.user.id).then(userResult =>
+        io.in(`lobby_${lobbyId}`).emit('userJoined', {
+          userId: userResult.rows[0].profile.id,
+          displayName: userResult.rows[0].profile.displayName,
+          avatar: userResult.rows[0].profile._json.avatar,
+          lobbyId
+        })).catch(console.log);
+    });
+  }
+
+  res.sendStatus(200);
+});
+      
+const checkQueue = async () => {
+  const queue = await db.getQueue();
+  setTimeout(() => {
+    const nextAcceptQueue = queue.rows.slice(0, 10);
+    const tempAcceptId = uuid();
+
+    nextAcceptQueue.forEach((player) => {
+      io.sockets.connected[player.socketid].join(tempAcceptId);
+    });
+
+    io.to(tempAcceptId).emit('acceptQueue', { acceptQueueId: tempAcceptId });
+  }, 3000);
+  /* if (queue.rows.length >= 10) {
+    const nextAcceptQueue = queue.rows.slice(0, 10);
+    const tempAcceptId = uuid();
+    nextAcceptQueue.forEach((player) => {
+      io.sockets.connected[player.socket].join(tempAcceptId);
+    });
+
+    io.of(tempAcceptId).emit('acceptQueue', { acceptQueueId: tempAcceptId });
+  } */
+}
 
 module.exports = app;
--- db.js
+++ db.js
@@ -13,25 +13,107 @@ const pool = new Pool(config);
 
 module.exports.query = (text, values) => pool.query(text, values);
 
-module.exports.getUser = identifier => pool.query('select * from dotaleague.user where identifier = $1', [identifier]);
+module.exports.getUser = identifier =>
+  pool.query('select * from dotaleague.user where identifier = $1', [identifier]);
 
-module.exports.createUser = (identifier, profile) => pool.query('insert into dotaleague.user (identifier, profile) values ($1, $2)', [identifier, profile]);
+module.exports.createUser = (identifier, profile) =>
+  pool.query('insert into dotaleague.user (identifier, profile) values ($1, $2)', [
+    identifier,
+    profile
+  ]);
 
-module.exports.setProfile = (identifier, profile) => pool.query('update dotaleague.user set profile = $1 where identifier = $2', [profile, identifier]);
+module.exports.setProfile = (identifier, profile) =>
+  pool.query('update dotaleague.user set profile = $1 where identifier = $2', [
+    profile,
+    identifier
+  ]);
 
 module.exports.getLobbies = () => pool.query('select * from dotaleague.lobby');
 
-module.exports.newMessage = (message, lobby, userId) => pool.query('insert into dotaleague.messages (content, lobbyid, userId) values ($1, $2, $3) returning *', [message, lobby, userId]);
+module.exports.newMessage = (message, lobby, userId) =>
+  pool.query(
+    'insert into dotaleague.messages (content, lobbyid, userId) values ($1, $2, $3) returning *',
+    [message, lobby, userId]
+  );
 
-module.exports.getMessages = lobbyId => pool.query('select * from dotaleague.messages where messages.lobbyid = $1', [lobbyId]);
+module.exports.getMessages = lobbyId =>
+  pool.query('select * from dotaleague.messages where messages.lobbyid = $1', [lobbyId]);
 
-module.exports.getLobbyMembers = lobbyId => pool.query('select * from dotaleague.user where lobbyid = $1', [lobbyId]);
+module.exports.getLobbyMembers = lobbyId =>
+  pool.query('select * from dotaleague.user where lobbyid = $1', [lobbyId]);
 
-module.exports.joinLobby = (lobbyId, userId) => pool.query('update dotaleague.user set lobbyid = $1 where identifier = $2 returning *', [lobbyId, userId]);
+module.exports.createLobby = () =>
+  pool
+    .query("insert into dotaleague.lobby (started, name) values (FALSE, 'test') returning id")
+    .then((result) => {
+      return result.rows[0].id
+    });
 
-module.exports.leaveLobby = userId => pool.query('update dotaleague.user set lobbyid = null where identifier = $1 returning *', [userId]);
+module.exports.joinLobby = (lobbyId, userId) =>
+  pool.query('update dotaleague.user set lobbyid = $1 where identifier = $2 returning *', [
+    lobbyId,
+    userId
+  ]);
+
+module.exports.leaveLobby = userId =>
+  pool.query('update dotaleague.user set lobbyid = null where identifier = $1 returning *', [
+    userId
+  ]);
+
+module.exports.checkCaptain = (userId, team) =>
+  pool
+    .query(
+      `select * from dotaleague.user
+      join dotaleague.lobby on lobby.id = dotaleague.user.lobbyid
+      where identifier = $1`,
+      [userId]
+    )
+    .then((result) => {
+      if (result.length === 1) {
+        const user = result[0];
+        if (user[`${team}_captain`] === userId) {
+          return true;
+        }
+      } else {
+        return false;
+      }
+    });
+
+module.exports.userOnTeam = userId =>
+  pool
+    .query(
+      `select * from dotaleague.user
+    where identifier = $1`,
+      [userId]
+    )
+    .then((result) => {
+      if (result.length === 1) {
+        const user = result[0];
+        if (user.team !== null) {
+          return true;
+        }
+        return false;
+      }
+      return false;
+    });
+
+module.exports.getQueue = () => pool.query('select * from dotaleague.queue');
+
+module.exports.addUserToQueue = (userId, socketId) =>
+  pool.query('insert into dotaleague.queue (userId, socketId) values($1, $2)', [userId, socketId]);
+
+module.exports.setAccepted = (userId, tempAcceptId) =>
+  pool.query('update dotaleague.queue set(accepted, acceptId) = (TRUE, $2) where userId = $1', [
+    userId,
+    tempAcceptId
+  ]);
+
+module.exports.getAccepted = tempAcceptId =>
+  pool.query('select * from dotaleague.queue where accepted = TRUE and acceptId = $1', [tempAcceptId]);
+
+module.exports.removeUserFromQueue = userId =>
+  pool.query('delete from dotaleague.queue where userId = $1', [userId]);
 
 module.exports.pg = pool.client;
 
 module.exports.pool = pool;
-
